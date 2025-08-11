91bc2f7a6459aad65d8668d8458b5d13377a5db4
Arthur Pereira <arthur.p31@aluno.ifsc.edu.br>
2025-08-11 09:06:03 -0300

    Created a clicker system

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.app;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,16 +12,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    TextView tv;
+    int x = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+        tv=findViewById(R.id.tv);
+        tv.setText("Clique me");
+        tv.setOnClickListener(v ->{tv.setText(String.valueOf(x));x++;});
 
-            return insets;
-        });
-    }
+    };
 }
