6d5cf3746563f4247f815cf1c5da378c355d6d63
Arthur <arthur.p31@aluno.ifsc.edu.br>
2025-08-25 08:31:13 -0300

    Lifecycle tester

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.app;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,4 +22,45 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    @Override
+    protected void onStart() {
+        Log.d("lifecycle","Start");
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("lifecycle","Pause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d("lifecycle","Restart");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d("lifecycle","Stop");
+
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("lifecycle","Destroy");
+
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("lifecycle","Resume");
+
+        super.onResume();
+    }
+
+
 }
