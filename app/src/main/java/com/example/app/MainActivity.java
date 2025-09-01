be0682504665c14462f0a4c45a2696366b59a67d
arthur <arthur.p31@aluno.ifsc.edu.br>
2025-09-01 09:03:31 -0300

    Implemented random

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.app;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,58 +13,37 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+    EditText editTextMin, editTextMax;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        button = findViewById(R.id.button);
+        editTextMin = findViewById(R.id.edMin);
+        editTextMax = findViewById(R.id.edMax);
+        tv = findViewById(R.id.tvResultado);
+
+        button.setOnClickListener(v -> {
+            Random r = new Random();
+            int min = 0, max = 1000;
+            int a;
+            try {
+                min = Integer.parseInt(editTextMin.getText().toString());
+                max = Integer.parseInt(editTextMax.getText().toString());
+                a = r.nextInt(min, max);
+            } catch (Exception e) {
+
+            }
+            try {
+
+            }
+            tv.setText(Integer.toString(a));
         });
     }
-
-    @Override
-    protected void onStart() {
-        Log.d("lifecycle","Start");
-        super.onStart();
-    }
-
-    @Override
-    protected void onPause() {
-        Log.d("lifecycle","Pause");
-        super.onPause();
-    }
-
-    @Override
-    protected void onRestart() {
-        Log.d("lifecycle","Restart");
-        super.onRestart();
-    }
-
-    @Override
-    protected void onStop() {
-        Log.d("lifecycle","Stop");
-
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        Log.d("lifecycle","Destroy");
-
-        super.onDestroy();
-    }
-
-    @Override
-    protected void onResume() {
-        Log.d("lifecycle","Resume");
-
-        super.onResume();
-    }
-
-
 }
