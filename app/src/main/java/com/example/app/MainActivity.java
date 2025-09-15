64d509e1bb035866262232a00da858845c378285
Arthur <arthur.p31@aluno.ifsc.edu.br>
2025-09-15 09:07:38 -0300

    Advanced on the IMC calculator

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,8 +22,14 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         button=findViewById(R.id.btnCalcular);
+        EditText campoPeso = findViewById(R.id.editPes);
+        EditText campoAlt = findViewById(R.id.editAlt);
         button.setOnClickListener(v -> {
             Intent intent = new Intent(this,ResultadoIMC.class);
+            Bundle bundle = new Bundle();
+            bundle.putDouble("peso",Double.parseDouble(campoPeso.getText().toString()));
+            bundle.putDouble("altura",Double.parseDouble(campoAlt.getText().toString()));
+            intent.putExtras(bundle);
             startActivity(intent);
         });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
--- app/src/main/java/com/example/app/ResultadoIMC.java
+++ app/src/main/java/com/example/app/ResultadoIMC.java
@@ -1,6 +1,9 @@
 package com.example.app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,11 +19,19 @@ public class ResultadoIMC extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        Intent i = getIntent();
+        Bundle b = i.getExtras();
+        Double peso = b.getDouble("peso");
+        Double altura = b.getDouble("altura");
+        TextView tvResultado = findViewById(R.id.textResultado);
+        ImageView imageView = findViewById(R.id.viewPerfil);
+
         setContentView(R.layout.activity_resultado_imc);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        tvResultado.setText(String.valueOf(peso/(altura*altura)));
     }
 }
