1450a64dbc23b52e80144e1837b26907239c3628
Arthur <arthur.p31@aluno.ifsc.edu.br>
2025-10-06 08:39:45 -0300

    Início da atividade da aula de list view dinâmico

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.app;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -11,10 +13,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    String[] nomes = new String[] {"César","José","Romolu","Samanta","Gustavo","Bilu"};
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
     ListView listView;
+
+    Button button;
+    TextView campoTexto;
+    ArrayList<String> nomesAdicionados = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +29,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listView=findViewById(R.id.lv);
+        button = findViewById(R.id.salvar);
+        campoTexto = findViewById(R.id.campo);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(),
+                android.R.layout.simple_list_item_1, android.R.id.text1, nomesAdicionados);
+        listView.setAdapter(adapter);
+        button.setOnClickListener(l ->  {nomesAdicionados.add(campoTexto.getText().toString()); adapter.notifyDataSetChanged();});
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(),
-                android.R.layout.simple_list_item_1, android.R.id.text1, nomes);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener((pa, v, po, i) -> {
-            Toast.makeText(getApplicationContext(), nomes[po], Toast.LENGTH_SHORT).show();
+
+        listView.setOnLongClickListener((pa, v, po, i) -> {
+
         });
     }
 }
