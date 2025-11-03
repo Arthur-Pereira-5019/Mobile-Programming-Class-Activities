7cf1bb76904edbffafe508513a6005d61c6a797f
Arthur <peneir20@gmail.com>
2025-11-03 09:09:17 -0300

    Created layout for saving data

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.app;
 import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +16,8 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     SQLiteDatabase db;
+
+    Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +32,18 @@ public class MainActivity extends AppCompatActivity {
         });
 
         db = openOrCreateDatabase("db", MODE_PRIVATE, null);
-        db.execSQL("CREATE TABLE notas (id INTEGER PRIMARY KEY AUTOINCREMENT, titulo VARCHAT, texto TEXT)");
+        db.execSQL("CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIMARY KEY AUTOINCREMENT, titulo VARCHAT, texto TEXT)");
         ContentValues cv = new ContentValues();
-        cv.put("Chega Natal hoho", "Minha primeira nota");
-        db.insert("notas",null, cv);
+
+
+        b = findViewById(R.id.button);
+        b.setOnClickListener(v -> {
+            EditText editText = findViewById(R.id.name);
+            String texto = editText.getText().toString();
+            cv.put("titulo", "Minha primeira nota");
+            cv.put("texto", texto);
+            db.insert("notas",null, cv);
+            Toast.makeText("Nota salva com sucesso!", Toast.LENGTH_SHORT).show();
+        });
     }
 }
