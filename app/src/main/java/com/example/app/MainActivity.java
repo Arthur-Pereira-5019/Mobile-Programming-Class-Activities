d8eb7e5c45474d06e1e91da69def16b2188437eb
Arthur <arthur.p31@aluno.ifsc.edu.br>
2025-11-10 08:50:52 -0300

    Added Listview adapter for extracting SQLite data

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.app;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -13,11 +16,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     SQLiteDatabase db;
 
     Button b;
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +38,10 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+
+        listView = findViewById(R.id.listView2);
         db = openOrCreateDatabase("db", MODE_PRIVATE, null);
-        db.execSQL("CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIMARY KEY AUTOINCREMENT, titulo VARCHAT, texto TEXT)");
+        db.execSQL("CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIMARY KEY AUTOINCREMENT, titulo VARCHAR, texto TEXT)");
         ContentValues cv = new ContentValues();
 
 
@@ -43,7 +52,33 @@ public class MainActivity extends AppCompatActivity {
             cv.put("titulo", "Minha primeira nota");
             cv.put("texto", texto);
             db.insert("notas",null, cv);
-            Toast.makeText("Nota salva com sucesso!", Toast.LENGTH_SHORT).show();
+            carregarListagem();
+            //Toast.makeText("Nota salva com sucesso!", Toast.LENGTH_SHORT).show();
         });
+
+        carregarListagem();
+    }
+
+    public void carregarListagem() {
+        ArrayList<String> titulos = new ArrayList<>();
+        Cursor cursor = db.rawQuery("SELECT * FROM notas", null);
+        cursor.moveToFirst();
+
+        while(!cursor.isAfterLast()) {
+            String titulo = cursor.getString(cursor.getColumnIndex("texto" +
+                    ""));
+            titulos.add(titulo);
+            cursor.moveToNext();
+
+        }
+
+        ArrayAdapter<String> titulosAdapter = new ArrayAdapter<>(
+        getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                titulos
+        );
+
+        listView.setAdapter(titulosAdapter);
+
     }
 }
